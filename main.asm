23604eac0ee0643c313fd381eeae755efbf1d400
Michel Eter <micoror1@gmail.com>
2018-02-13 10:09:10 -0800

    Master file with prefix, 32-bit.

---
+++ main.asm
@@ -0,0 +1,25 @@
+; Program Description: [Insert Description Here]
+; Author: Michel Eter
+; Creation Date: [Insert Creation Date]
+; Revisions: [Insert Any Revisions]
+; Date:              Modified by: Michel Eter
+; Operating System: Mint Linux
+; IDE/Compiler: Geany
+
+bits 32
+section .data  ;Initialized data definitions go here
+
+section .bss   ;Uninitialized memory reservations go here
+
+section .text
+
+global _start
+_start:
+  nop
+  ;Code starts here
+
+  ;Code ends here
+  nop
+  mov eax,1 ; Exit system call value
+  mov ebx,0 ; Exit return code
+  int 80h ; Call the kernel
