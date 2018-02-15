3c8aa6f8a5ee18a88614c8e3561959c33b341edc
Michel Eter <micoror1@gmail.com>
2018-02-14 17:37:07 -0800

    Added Linux function libraries.

--- main.asm
+++ main.asm
@@ -6,8 +6,16 @@
 ; Operating System: Mint Linux
 ; IDE/Compiler: Geany
 
+%include "./functions.inc"
+
 bits 32
 section .data  ;Initialized data definitions go here
+    A   db  2
+    B   dw  3
+    C   dd  4
+    D   dd  0h
+    eq1 db  "A+(B+C)=D", 0dh, 0ah, 0h
+    eq2 db  "(A+C)-B=D", 0dh, 0ah, 0h
 
 section .bss   ;Uninitialized memory reservations go here
 
@@ -17,7 +25,30 @@ global _start
 _start:
   nop
   ;Code starts here
+    push eq1
+    call PrintString
+    push eq2
+    call PrintString
+    call Printendl
 
+    
+    mov eax, 0h
+
+    mov ax, [B]
+    add eax, [C]
+    add al, [A]
+    mov [D], eax
+
+    call PrintRegisters
+
+    mov eax, 0
+
+    mov al, [A]
+    add eax, [C]
+    sub ax, [B]
+    mov [D], eax
+
+    call PrintRegisters
   ;Code ends here
   nop
   mov eax,1 ; Exit system call value
